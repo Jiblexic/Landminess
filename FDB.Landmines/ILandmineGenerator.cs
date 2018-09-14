28b26e7b3410a4c9f23b327b5fd0ff7958fd5e6b
Daniel Regan <d.regan@girlings.co.uk>
2018-09-14 16:01:02 +0100

    d

---
+++ FDB.Landmines/ILandmineGenerator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FDB.Landmines
+{
+    interface ILandmineGenerator
+    {
+        List<int[,]> LandminePlaceholder { get; set; }
+
+        List<int[,]> LandmineLocations();
+    }
+}
---
+++ FDB.Landmines/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FDB.Landmines
+{
+    class Program
+    {
+        private const int size = 8;
+
+        private static int[,] chessboard;
+        private static int[,] playerPosition;
+        private static int xPosition = 0;
+        private static int yPosition = 7;
+
+
+        private static void Main()
+        {
+
+            Game(null);
+        }
+
+        private static void Game(char? move)
+        {
+            Console.Clear();
+
+            if (move.HasValue)
+            {
+                if (move.Value == 'w')
+                {
+                    if(yPosition > 0) 
+                        yPosition--;
+
+                }
+                if (move.Value == 'a')
+                {
+                    if (xPosition > 0)
+                        xPosition--;
+                }
+
+                if (move.Value == 's')
+                {
+                    if (yPosition < 7)
+                        yPosition++;
+                }
+
+                if (move.Value == 'd')
+                {
+                    if (xPosition < 7)
+                        xPosition++;
+                }
+            }
+
+            //init chessboard
+            chessboard = new int[size, size];
+
+            playerPosition = new int[xPosition, yPosition];
+
+
+            for (int y = 0; y < size; y++)
+            {
+                for (int x = 0; x < size; x++)
+                {
+                    Console.Write("|{0}", xPosition == x && yPosition == y ? 'X' : ' ');
+                }
+
+                Console.WriteLine("|");
+            }
+
+            if (yPosition == 0)
+            {
+                Console.WriteLine("You Win!");
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("Landmines hit: 0");
+            var myMove = Console.ReadKey();
+
+            Game(myMove.KeyChar);
+        }
+    }
+}
